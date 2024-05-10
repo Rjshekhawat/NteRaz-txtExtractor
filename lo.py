6df071c14ba0e1b13ca15352e52567cd811af1c4
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-05-10 06:29:53 +0530

    Update lo.py

--- lo.py
+++ lo.py
@@ -70,9 +70,9 @@ def pull_run(work, cmds):
 	        print(result) 
 bot = Client(
   "CW",
-  bot_token= "7043797215:AAFPaQ6-09l-3JcYKJI71XFbAqEswkiwF0A",
-  api_id= 26181533 ,
-  api_hash= "61f8a38f9d9d079100ba56fcbb9ea9b7" 
+  bot_token= "6917084113:AAFOK37UIoEQLtY2HYu5UeF77cIF4BfGnwk",
+  api_id= 27097807 ,
+  api_hash= "9fd790a9cb1f639c921d941621d2959d" 
 )
 
 @bot.on_message(filters.command(["down"]) & ~filters.edited)
